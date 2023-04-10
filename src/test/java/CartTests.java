32aa1696e89f9400f2cdb0cfd5c53592b305a718
IRENE <mani16irene@gmail.com>
2023-04-10 16:06:31 -0400

    Irene's Commit

--- src/test/java/CartTests.java
+++ src/test/java/CartTests.java
@@ -3,13 +3,13 @@ import org.testng.annotations.Test;
 
 public class CartTests extends CoreShell {
 
-    @Test (priority = 1, groups = {"Sanity"})
+    @Test (priority = 1)
     // To validate add to cart functionality.
     public void testAddToCart() {
         actions.AddToCart();
     }
 
-    @Test (priority =2)
+    @Test (priority =2, groups = {"Sanity"})
     // To validate continue shopping functionality.
     public void testContinueShopping() {
         actions.ContinueShopping();
